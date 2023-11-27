31053290183b9efbbbca967c1d5f93388465c09c
wajidhussain001 <147056326+wajidhussain001@users.noreply.github.com>
2023-11-27 16:07:59 +0530

    Add files via upload

---
+++ python_task5/python tsk5.py
@@ -0,0 +1,25 @@
+#Python list
+fruits = ["apple","banana","cherries"]
+print(fruits)
+print(fruits[1])
+fruits.append("grapes")
+print(fruits)
+
+#python tuples
+veggies = ("carrot","raddish","beetroot")
+print(veggies)
+print(veggies[0])
+
+#python set
+vegetables = {"tomato","lettuce","capsicum"}
+print(vegetables)
+for temp in vegetables:
+ print(temp)
+
+#python dict
+thedict = {
+  "fruits1":"apple",
+  "fruits2":"banana",
+  "fruits3":"grapes",
+}
+print(thedict["fruits1"])
